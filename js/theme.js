46f193c0223688a554712e66159885a6518f5a20
yanylyy <yana.he@mail.ru>
2026-05-27 10:42:39 +0300

    1

--- js/theme.js
+++ js/theme.js
@@ -1,41 +1,39 @@
-$(() => {
+$(document).ready(() => {
     const SCHEMA_LIGHT = 'light';
     const SCHEMA_DARK = 'dark';
     const pathCss = '/css';
     let currentSchema;
 
-    const getSchema = () => currentSchema = localStorage.getItem('schema');
+    const getSchema = () => localStorage.getItem('schema');
     const setSchema = (schema) => localStorage.setItem('schema', schema);
 
     const getFileSchema = () => `${currentSchema}-theme.css`;
 
     const loadCss = (file) => {
         if (file) {
+            // Правильный селектор без #
             $("#theme-css").remove();
             $('<link>')
                 .attr({
-                    id: "#theme-css",
+                    id: "theme-css",  // Без # !
                     rel: "stylesheet",
                     href: `${pathCss}/${file}?t=${Date.now()}`
                 })
-                .appendTo('head')
+                .appendTo('head');
         }
-
     }
 
     $('.toggle').on('click', () => {
-        currentSchema = currentSchema === SCHEMA_LIGHT ? SCHEMA_DARK : SCHEMA_LIGHT
-        setSchema(currentSchema)
-        loadCss(getFileSchema())
+        currentSchema = currentSchema === SCHEMA_LIGHT ? SCHEMA_DARK : SCHEMA_LIGHT;
+        setSchema(currentSchema);
+        loadCss(getFileSchema());
     });
 
-    (() => {
-        currentSchema = getSchema();
-        if (!currentSchema) {
-            currentSchema = SCHEMA_LIGHT;
-            setSchema(currentSchema);
-        }
-        loadCss
-            (getFileSchema());
-    })()
-})
+    // Инициализация
+    currentSchema = getSchema();
+    if (!currentSchema) {
+        currentSchema = SCHEMA_LIGHT;
+        setSchema(currentSchema);
+    }
+    loadCss(getFileSchema());
+});
